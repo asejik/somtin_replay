b5eb4cce64e68cb254fe0c10163eefb24e0605d0
Sogo Ayenigba <sogoayenigba@gmail.com>
2022-02-11 13:57:28 +0000

    App bar done

---
+++ lib/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text('Somtin Somtin'),
+      ),
+      body: Column(),
+    );
+  }
+}
