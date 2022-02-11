faafc79b9282015d1a8fbd731570621661f8b008
Sogo Ayenigba <sogoayenigba@gmail.com>
2022-02-11 14:40:31 +0000

    Added the first widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,10 +5,40 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.pink,
         title: Text('Somtin Somtin'),
       ),
-      body: Column(),
+      body: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int position) {
+          return _balanceView();
+        },
+      ),
     );
   }
+}
+
+Widget _balanceView() {
+  return Row(
+    children: [
+      Expanded(
+          child: Container(
+        padding: EdgeInsets.only(left: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Total Gift Voucher Value"),
+            Text("GHS 100"),
+            Text("BUY GIFT VOUCHER"),
+          ],
+        ),
+      )),
+      Icon(
+        Icons.picture_in_picture_alt,
+        size: 80,
+      )
+    ],
+  );
 }
